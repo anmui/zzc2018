ff515b9e37a96f54d03ae227d0e0596d1adb65b0
anmui <893724023@qq.com>
2018-09-03 21:27:26 +0800

    a.c

--- a.c
+++ a.c
@@ -1,6 +1,7 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 #include<pthread.h>
 #include<semaphore.h>
 #include<sys/ipc.h>
@@ -11,6 +12,7 @@
 
 
 int idAs,idBs,idAr,idBr,Asem,Bsem;
+char fflag[10];char w[10];
 
 
 int P(int sem_id)
@@ -82,51 +84,111 @@ int main()
 	char ch[10];	
 	int flag;	
 	};
+	struct tags
+	{
+	char ch[10];
+	char flag[10];
+	};
+	struct tags tag[100];
 	int size=5*sizeof(struct token);
 	int pid1,pid2;
-	int i=0,j=0;
+	int i=0,j=0,z=0;
 	char c;
 	pid1 = shmget((key_t)7,size,IPC_CREAT|0777);
 	pid2 = shmget((key_t)8,size,IPC_CREAT|0777);
-	struct token w;
+	
 	struct token *add1,*add2;
 	
 	add1 = (struct token *)shmat(pid1,0,0);
 	add2 = (struct token *)shmat(pid2,0,0);
 	add1->flag=0;add2->flag=0;
+	
+	int number;
 	while(1)
 	{
 	
 		printf("please input the order: \n");
 		scanf("%c",&c);
-		if(c=='r'&&add1[i].flag==1)
+		if(c=='r'&&add1[i].flag==1)//接受信息
 		{
 		P(idAr);
 		P(Asem);
 		printf("port a receive mail %s sucessfully\n",add1[i].ch);
 		add1[i].flag=0;
-		i=(i+1)%5;
 		V(Asem);
 		V(idBs);
+		printf("would you want to give this mail a tag?(yes/no)\n");
+		scanf("%s",fflag);
+		if(strcmp(fflag,"yes")==0)
+		{
+			strcpy(tag[z].ch,add1[i].ch);
+			printf("please input the tag:\n");
+			scanf("%s",w);
+			strcpy(tag[z].flag,w);
+			z=(z+1)%100;
+		}
+		i=(i+1)%5;
 		}
 		else if(c=='r'&&add1[i].flag==0)
 		{
 		printf("waiting..\n");
 		}
-		if(c=='s')
+		else if(c=='s')//发送信息
 		{
 			printf("input the content: \n");
 			P(idAs);
 			P(Bsem);
-			scanf("%s",add2[j].ch);
+			scanf("%s",add2[j].ch);	
 			add2[j].flag=1;
 			printf("send mail %s sucessfully\n",add2[j].ch);
 			j=(j+1)%5;
 			V(Bsem);
 			V(idBr);
+			
 		}
-		if(c=='e')
+		else if(c=='f')//按tag查找信息
+		{
+			printf("please input the tag you want to find\n");
+			char tag1[10];
+			scanf("%s",tag1);
+			int i1=0,findflag=0;
+			while(i1<=z)
+			{
+				if(strcmp(tag1,tag[i1].flag)==0)
+				{
+					printf("the mail is %s\n",tag[i1].ch);
+					findflag=1;
+					break;
+				}
+				i1++;
+			}
+			if(findflag==0)
+			{
+				printf("can't find\n");
+			}
+		}
+		else if(c=='h')//查找历史纪录（前5次收信）
+		{
+			int m=i-1;	
+			printf("please input the number:\n");
+			scanf("%d",&number);
+			if(number>=5)
+			{
+				printf("can't find\n");
+			}
+			else
+			{	
+				if(number-i>=0)
+					printf("the mail is %s\n",add1[i-number].ch);
+			        else
+					printf("the mail is %s\n",add1[i-number+5].ch);
+			}
+		}
+
+		else if(c=='e')
 		break;
+		else
+		{}
 		getchar();
 	}
 }
